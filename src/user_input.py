5bbdd5690427b1b7a587d8146bbfb6f62d5ade85
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2023-12-07 14:40:44 -0500

    started working on user_input with argparse

--- src/user_input.py
+++ src/user_input.py
@@ -1,2 +1,21 @@
-# get user input and call commands
-# probably uses argparse to parse cli input and call correct commands
+import argparse
+import sys
+sys.path.append('src')
+from commands import AddEntryCommand, QueryEntriesCommand
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Time Tracker CLI App")
+    parser.add_argument('command', choices=['record', 'query'], help='Desired command to execute')
+    parser.add_argument('date', help='Date in format YYYY/MM/DD')
+    parser.add_argument('start_time', help='Time started in format HH:MM AM/PM')
+    parser.add_argument('end_time', help='Time ended in format HH:MM AM/PM')
+    parser.add_argument('task', help='Task completed')
+    parser.add_argument('tag', help='Tag for the task in :TAG format')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    if args.command == 'record':
+        command = AddEntryCommand(args)
