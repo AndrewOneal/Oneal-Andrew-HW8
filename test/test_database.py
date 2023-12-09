d7539251346c7cc1d0116b2e4dd94aa6659cc9da
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2023-12-09 06:32:34 -0500

    added test for db addEntry

--- test/test_database.py
+++ test/test_database.py
@@ -1,5 +1,6 @@
 import pytest
 import sys
+import sqlite3
 sys.path.append('src')
 from database import DatabaseController
 
@@ -7,14 +8,44 @@ from database import DatabaseController
 def db():
     yield DatabaseController()
 
-def test_init(db):
-    pass
-
 def test_addTable(db):
-    pass
+    db.addTable()
 
-def test_addEntry(db):
-    pass
+def test_add_entry(db):
+    db.deleteAll()
+
+    date = "2023/01/01"
+    start_time = "10:00AM"
+    end_time = "11:00AM"
+    task = "Sample Task"
+    tag = ":SAMPLETAG"
+
+    db.addEntry(date, start_time, end_time, task, tag)
+
+    entries = db.queryAllEntries()
+    assert len(entries) == 1
+    assert entries[0][1] == date
+    assert entries[0][2] == start_time
+    assert entries[0][3] == end_time
+    assert entries[0][4] == task
+    assert entries[0][5] == tag
+
+    date = "today"
+    start_time = "10:00"
+    end_time = "11:00"
+    task = "Sample Task"
+    tag = ":SAMPLETAG"
+
+    db.addEntry(date, start_time, end_time, task, tag)
+    entries = db.queryAllEntries()
+
+    assert len(entries) == 2
+    assert entries[1][1] == date
+    assert entries[1][2] == start_time
+    assert entries[1][3] == end_time
+    assert entries[1][4] == task
+    assert entries[1][5] == tag
+
 
 def test_queryAllEntries(db):
     pass
